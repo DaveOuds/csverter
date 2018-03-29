1734c146896985b5913a8ca39e1ca66d86b4b216
Davey Oudshoorn (admin) <doudshoorn-a@doudshoorn-l1.ffx.bixal.net>
2018-03-29 09:27:35 -0400

    forgot to add notes and tagManager files

---
+++ src/components/Notes.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default class Notes extends React.Component{
+  render(){
+    return(
+      <div className='notepadpp'>
+        {this.renderNotes()}
+
+      <form ref='noteForm' onSubmit={(evt) => this.handleSubmit(evt)}>
+          <input type="text" ref="note" placeholder="note"/>
+          <input type="submit" hidden/>
+        </form>
+      </div>
+    )
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    var profile = this.props.profile;
+    profile.notes.push(this.refs.note.value);
+    this.props.add_note(profile);
+    this.refs.noteForm.reset();
+ }
+
+  renderNotes(){
+    return(
+      <ul className='notes'>
+        {this.props.notes.map((note, i) =>{
+          return(
+            <li className='note' key={i} id={i}>
+              {note}
+              <button className="remove-note" onClick={() => this.handleRemove(i)}> delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  handleRemove(i){
+    var profile = this.props.profile;
+    profile.notes.splice(i, 1);
+    this.props.remove_tag(profile);
+  }
+}
